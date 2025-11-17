bad29fc13e01efb1792d28bb25bf42a021365453
PedroNunes <162641504+PedroNunesc@users.noreply.github.com>
2025-11-17 15:51:12 -0300

    Create script to initialize DB and add admin user
    This script initializes the database and creates an admin user.

---
+++ src/createAdmin.ts
@@ -0,0 +1,16 @@
+import { AppDataSource } from './config/data-source';
+import { User } from "./models/User";
+
+async function createAdmin() {
+    await AppDataSource.initialize();
+
+    const user = new User("Admin", "admin@gmail.com", "root");
+    user.role = "admin";
+    await AppDataSource.manager.save(user);
+
+    console.log("Admin criado com sucesso!");
+    process.exit(0);
+}
+
+createAdmin().catch(console.error);
+//arquivo
